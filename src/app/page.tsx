96b86406889eceed7c8115479b52c2cd18dd6e51
RayFian <ray.fian7886@gmail.com>
2024-04-05 09:18:04 +0700

    Praktikum 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Tombol_1 from "@/components/button";
 
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,24 @@
-export default function Tombol_1(){
-    return(
-        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">
+export default function Tombol_1() {
+    function handleClick() {
+        alert("Tombol telah ditekan!!");
+    }
+
+    function handleMouseOver() {
+        alert("Eits, mau mencet tombol ya??");
+    }
+
+    function handleMouseLeave() {
+        alert("Loh, kok udah pergi!!!");
+    }
+
+    return (
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={handleClick}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+        >
             Ini tombol
         </button>
     );
-
 }
