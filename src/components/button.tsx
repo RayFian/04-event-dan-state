ba47fdc6787be338f97abeb348cae344e5fecf8c
RayFian <ray.fian7886@gmail.com>
2024-04-05 11:17:26 +0700

    Praktikum 2

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -22,3 +22,13 @@ export default function Tombol_1() {
         </button>
     );
 }
+
+export function Tombol_2({isiPesan, namaTombol}){
+    return(
+        <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={() => alert(isiPesan)}>
+            {namaTombol}
+        </button>
+    );
+}
